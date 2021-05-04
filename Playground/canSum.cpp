8e0977243ba0afe2419f5144b64b3dc2c0519fe2
Ray <zzy1124983737@gmail.com>
2021-05-04 18:10:41 -0400

    update 8

--- Playground/canSum.cpp
+++ Playground/canSum.cpp
@@ -3,6 +3,52 @@
 #include <map>
 using namespace std;
 
+bool canSum(int n, vector<int> a) {
+	if (n == 0)
+	{
+		return true;
+	}
+	if (n < 0)
+	{
+		return false;
+	}
+	
+	for (int i = 0; i < a.size(); i++) {
+		if (canSum(n - a[i], a)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool dp_canSum(int n, vector<int> a,map<int, bool> &memo) {
+	if (memo.find(n) != memo.end()) {
+		return memo[n];
+	}
+	if (n == 0)
+	{
+		return true;
+	}
+	if (n < 0)
+	{
+		return false;
+	}
+
+	for (int i = 0; i < a.size(); i++) {
+		if (dp_canSum(n - a[i], a, memo)) {
+			memo[n] = true;
+			return true;
+		}
+	}
+	memo[n] = false;
+	return false;
+}
 
 
-
+int main()
+{
+	vector<int> a = { 7,14 };
+	map<int, bool> m;
+	cout << dp_canSum(300, a, m) << endl;
+	//cout << canSum(300, a) << endl;
+}
